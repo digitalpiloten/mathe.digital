00656a23694d36c4c0c129114a278b7d3e9b0504
MagicMarvMan <marvin.kai@gmx.de>
2019-07-19 12:46:00 +0200

    Add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import logo from '../assets/images/logo.svg';
+import logo from '../assets/images/ZFM_MADI_ICON.jpg';
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <span className="logo"><img src={logo} alt="" /></span>
+        <span className="logo"><img className="App-logo" src={logo} alt="" /></span>
         <h1>MatheDigital</h1>
         <p>Willkommen beim Projekt MatheDigital des Zentrum für Mathematik e.V.: Programmieren mit dem Calliope Mini.</p>
     </header>
