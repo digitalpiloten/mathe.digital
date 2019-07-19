ed3223a88fceed600c9d845f984a08aa599291e3
MagicMarvMan <marvin.kai@gmx.de>
2019-07-19 18:38:41 +0200

    Add privacy policy

---
+++ src/components/HeaderPrivacyPolicy.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const HeaderPrivacyPolicy = (props) => (
+    <header id="header">
+        <h1>Datenschutzerklärung</h1>
+        <Link to="/" className="button">
+            Zurück
+        </Link>
+    </header>
+)
+
+export default HeaderPrivacyPolicy
---
+++ src/pages/datenschutz.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+import Layout from '../components/layout'
+import HeaderPrivacyPolicy from '../components/HeaderPrivacyPolicy'
+import PplContent from '../components/PplContent'
+
+class PrivacyPolicy extends React.Component {
+  render() {
+
+    return (
+      <Layout>
+        <Helmet title="Datenschutzerklärung" />
+        <HeaderPrivacyPolicy />
+        <div id="main">
+          <section id="content" className="main">
+            <PplContent />
+          </section>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default PrivacyPolicy
