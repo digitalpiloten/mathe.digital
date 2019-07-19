944cb7eb31eed5d93079783dbe57e21946f65e69
MagicMarvMan <marvin.kai@gmx.de>
2019-07-19 20:21:41 +0200

    Remove old pic import

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ import Waypoint from 'react-waypoint'
 import Layout from '../components/layout'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
-import pic01 from '../assets/images/pic01.jpg'
+
 
 class Index extends React.Component {
   constructor(props) {
